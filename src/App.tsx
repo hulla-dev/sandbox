b5be02afd29026dce1a5d6388ea101e64387e1ce
samuelhulla <oficialny.hulla@gmail.com>
2020-10-09 18:37:57 +0200

    Node modules reinstall

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import { useField } from 'eformless'
 import './App.css';
 
 function App() {
+
+  // Add this in your component file
+require('react-dom');
+// @ts-ignore
+window.React2 = require('react');
+// @ts-ignore
+console.log(window.React1 === window.React2);
   const [test, handleChange] = useField('testing')
   return (
     <input type="text" value={test.value} onChange={handleChange} />
